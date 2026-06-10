f99b7bbf2b04555f3eee00cc0dc99fe820d904c3
aiya <aichkaxxx2007@gmail.com>
2026-06-11 00:05:40 +0500

    models defined

--- task_planner/planner/models.py
+++ task_planner/planner/models.py
@@ -1,4 +1,49 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
+
+class Project(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title =models.CharField(max_length=255) 
+    description=models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Task(models.Model):
+    class Status(models.TextChoices):
+        TODO = "todo", "Todo"
+        IN_PROGRESS = "in_progress", "In progress"
+        DONE = "done", "Done"
+
+    class Priority(models.TextChoices):
+        LOW = "low", "Low"
+        MEDIUM = "medium", "Medium"
+        HIGH = "high", "High"
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title=models.CharField(max_length=255)
+    description=models.TextField(blank=True)
+
+    status=models.CharField(
+        max_length=20, 
+        choices=Status.choices, 
+        default=Status.TODO)
+    priority=models.CharField(
+        max_length=20, 
+        choices=Priority.choices, 
+        default=Priority.MEDIUM)
+
+    deadline =models.DateField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    author= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text =models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
